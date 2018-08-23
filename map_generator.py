89505973c782d0cdde8004341b3655cec1729cf1
csdhv9 <csdhv9@mst.edu>
2018-08-23 12:33:57 -0500

    Add basic oscillation in generator.

--- map_generator.py
+++ map_generator.py
@@ -4,6 +4,8 @@ from cell import Cell
 
 from vars_constant import section_l
 
+from math import sin
+
 
 class Generator(object):
     def __init__(self, display):
@@ -15,15 +17,26 @@ class Generator(object):
         # Every section_l blocks there will be a block chosen by a certain number in the seed fed into a formula
         # the blocks around these pivotal blocks will be chosen by a voting system of the pivotal blocks that
         # have wants based on the seed and influence based on their distance from the block being selected
-        if not x % section_l and not y % section_l:
-            # is pivotal block
-            state = 5
-        else:
-            if (x + y) % 2:
-                # is odd block
-                state = 4
+        if not x % section_l and not y % section_l:  # is pivotal block
+            # Will use oscillating function with weights determined by the seed to chose pivotal blocks
+
+            oscillator = 100 * sin((x % 100) + (y % 100)) / ((x % 100) + (y % 100) if not (x % 1) + (y % 100) == 0 else 44)
+
+            oscillator = abs(oscillator)
+
+            if oscillator < 1:
+                state = 0
+            elif oscillator < 5:
+                state = 1
+            elif oscillator < 10:
+                state = 2
             else:
-                # is even block
+                state = 3
+
+        else:
+            if (x + y) % 2:  # is odd block
+                state = 4
+            else:  # is even block
                 state = 6
 
         return state
