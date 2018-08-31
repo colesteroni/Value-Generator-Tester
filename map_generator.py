49a88d555a71f7cc013e0b7275f09e8c8ba49cc3
csdhv9 <csdhv9@mst.edu>
2018-08-30 22:05:51 -0500

    Filler blocks now generated.

--- map_generator.py
+++ map_generator.py
@@ -3,9 +3,10 @@
 # the blocks around these pivotal blocks will be chosen by a voting system of the pivotal blocks that
 # have wants based on the seed and influence based on their distance from the block being selected
 
+from vars_constant import state_dict
 import vars_global
 
-from math import sin
+from math import sin, sqrt
 
 
 def pivotal_oscillator(x, y):
@@ -42,12 +43,44 @@ def filler_state(x, y):
     y1 = y - dy
     y2 = y + (vars_global.y_pivotal_gap - dy) if dy else None
 
-    surrounding_pivotals = [(x1, y1)]
-    surrounding_pivotals += [(x2, y1)] if dx else []
-    surrounding_pivotals += [(x1, y2)] if dy else []
-    surrounding_pivotals += [(x2, y2)] if dx and dy else []
+    surrounding_pivotals = [[(x1, y1)]]
+    surrounding_pivotals += [[(x2, y1)]] if dx else []
+    surrounding_pivotals += [[(x1, y2)]]if dy else []
+    surrounding_pivotals += [[(x2, y2)]] if dx and dy else []
 
-    return 4
+    for pivotal in surrounding_pivotals:
+        if x - pivotal[0][0] and y - pivotal[0][1]:
+            pivotal.append(sqrt((x - pivotal[0][0])**2 + (y - pivotal[0][1])**2))
+        elif x - pivotal[0][0]:
+            pivotal.append(abs(x - pivotal[0][0]))
+        else:  # y - pivotal[0][1]:
+            pivotal.append(abs(y - pivotal[0][1]))
+
+    # vote with weights based on distance & block weights
+    votes = [[key] for key in state_dict]
+
+    for pivotal in surrounding_pivotals:
+        index = None
+
+        for i in range(0, len(votes)):
+            if votes[i][0] is pivotal_state(pivotal[0][0], pivotal[0][1]):
+                index = i
+                break
+
+        try:
+            votes[index][1] += 1 * pivotal[1]
+        except IndexError:
+            votes[index].append(1 * pivotal[1])
+
+    state = 0
+    max_vote = 0
+    for vote in votes:
+        if len(vote) > 1:
+            if vote[1] > max_vote:
+                max_vote = vote[1]
+                state = vote[0]
+
+    return state
 
 
 def get_state(x=None, y=None, cell=None):
