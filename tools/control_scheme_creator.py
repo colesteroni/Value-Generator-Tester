fe1252057208eae4a26e11a76b20e2f35ef82ad3
csdhv9 <csdhv9@mst.edu>
2018-09-03 22:30:07 -0500

    Updated control_scheme_creator.

--- tools/control_scheme_creator.py
+++ tools/control_scheme_creator.py
@@ -1,8 +1,15 @@
+# PLEASE NOTE: Can only run tools on command line from inside tools directory
+
 import pickle
 
-from controller import default_control_scheme
+import sys
+sys.path.append("..")
+
+import controller
 
 slot = 1
+
+default_control_scheme = controller.default_control_scheme
 
 with open('../data/controller_schemes/{}.dat'.format(slot), 'wb') as file:
     pickle.dump(default_control_scheme, file)
